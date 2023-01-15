a3b4f6f1ce7c735aaef5ca629f204ae80429dfdd
Dimas <dimassukarso@gmail.com>
2023-01-15 23:59:13 +0700

    init

---
+++ src/routes/custom/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	let text = 'Halo';
+	$: if (text.toLowerCase() === 'dimas') {
+		text = 'Blokkk';
+	}
+</script>
+
+<svelte:head>
+	<title>Custom Page</title>
+	<meta name="description" content="Custom page" />
+</svelte:head>
+
+<div class="text-column">
+	<h1 class="text">{text}</h1>
+	<input type="text" bind:value={text} />
+</div>
+
+<style>
+	.text {
+		color: aqua;
+	}
+</style>
---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	// import Product from 'src/components/products/+product.svelte';
+	import Products from '../../components/products/+products.svelte';
+	import { onMount } from 'svelte';
+	import type { ProductsInterface } from '../../interfaces';
+
+	let products: ProductsInterface;
+
+	async function getProducts() {
+		const res = await fetch('https://dummyjson.com/products', {
+			method: 'GET'
+		});
+
+		products = await res.json();
+	}
+
+	onMount(getProducts);
+</script>
+
+<svelte:head>
+	<title>Products</title>
+	<meta name="products" content="Products" />
+</svelte:head>
+
+{#if products}
+	<div class="h-auto">
+		<Products data={products} />
+	</div>
+{:else}
+	<p>Loading ...</p>
+{/if}
+
+<style>
+</style>
